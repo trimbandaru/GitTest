b3b87e61c78ebc456567c5ddccec5832102b9680
trimbandaru <trimurthulu.bandaru@gmail.com>
2017-10-02 17:27:38 +0530

    test github

--- app/src/main/java/com/bandarufoods/gittest/MainActivity.java
+++ app/src/main/java/com/bandarufoods/gittest/MainActivity.java
@@ -34,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
+
+        //test
     }
 
     @Override
